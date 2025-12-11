1a63adf3683cce1b3e3a633259fd7d0c7a21b810
Anjishnu Banerjee <inviktor27@gmail.com>
2025-12-11 22:05:46 +0530

    Add initial sqlite db

---
+++ internal/store/db_store.go
@@ -0,0 +1,54 @@
+package store
+
+import (
+	"database/sql"
+	_ "modernc.org/sqlite"
+)
+
+type DBStore struct {
+	db *sql.DB
+}
+
+const CREATE_DEADLINES_TABLE = `
+CREATE TABLE IF NOT EXISTS deadlines(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT NOT NULL,
+	datetime TEXT NOT NULL
+);`
+
+const CREATE_BASKETS_TABLE = `
+CREATE TABLE IF NOT EXISTS baskets(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT UNIQUE NOT NULL
+);`
+
+const CREATE_PINS_TABLE = `
+CREATE TABLE IF NOT EXISTS pins(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	content TEXT NOT NULL,
+	basket_id INTEGER NOT NULL,
+	FOREIGN KEY(basket_id) REFERENCES baskets(id) 
+		ON DELETE CASCADE
+);`
+
+func mustExec(db *sql.DB, query string) {
+	if _, err := db.Exec(query); err != nil {
+		panic(err)
+	}
+}
+
+func NewDBStore(path string) (*DBStore, error) {
+	db, err := sql.Open("sqlite", path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	mustExec(db, "PRAGMA journal_mode = WAL;")
+	mustExec(db, "PRAGMA foreign_keys = ON;")
+	mustExec(db, CREATE_DEADLINES_TABLE)
+	mustExec(db, CREATE_BASKETS_TABLE)
+	mustExec(db, CREATE_PINS_TABLE)
+
+	return &DBStore{db: db}, nil
+}
